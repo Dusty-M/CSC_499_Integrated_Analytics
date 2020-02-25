49a768f47b5f5ba1cfa9fbbfe050ae2d532bf967
Dusty Miller <leodmiller@gmail.com>
2020-02-24 18:53:32 -0800

    Using boost::program_options to handle args
    - Linked boost library via cmake and included boost/program_options.hpp in main.cpp
    - Switched from manually handling user args to using program_options instead
    - CSVParser_tester is now called differently.  An example follows:
    $ bin/CSVParser_tester --filename census_2000_sample_places_sample_dimensions.csv -- header SE_T001_001 --header_row_index 2 --first_data_row_index 1

--- app/main.cpp
+++ app/main.cpp
@@ -5,16 +5,52 @@
 #include <iostream>
 #include <memory> // shared pointers
 #include <unistd.h> // usleep
-void usage() {
-	std::cout << "****************************************************" << std::endl;
-	std::cout << "Error: too few args.  Proper usage:\n"
-		<< "./test_prog <CSV filename> "
-		<< "<header name> <index of header> <index of first data>\n"
-		<< "example: test_prog some_data.csv SE_T001_001 1 2\n";
-	std::cout << "****************************************************" << std::endl;
-}
+#include <boost/program_options.hpp>
 
 int main( int argc, char **argv ) {
+
+	// program arg variables
+	std::string 	filename;
+	std::string 	header;
+	uint32_t header_row_index;
+	uint32_t  first_data_row_index;
+
+	namespace po = boost::program_options;
+	po::options_description desc("Options");
+	desc.add_options()
+		("help", "Print help messages")
+		("filename", po::value<std::string>(&filename)->required(), 
+			"CSV filename including file extension")
+		("header", po::value<std::string>(&header)->required(), 
+			"unique header of column")
+		("header_row_index", po::value<uint32_t>(&header_row_index)->required(), 
+			"index of row header is located on")
+		("first_data_row_index", po::value<uint32_t>(&first_data_row_index)->required(), 
+			"index of first row of actual data");
+	
+	po::variables_map vm;
+	try{
+		po::store(po::parse_command_line(argc, argv, desc), vm);
+
+		if (vm.count("help") )
+      { 
+        std::cout << "Progressive Analytics Application" << std::endl 
+                  << desc << std::endl; 
+        return 0; 
+      } 
+ 
+      po::notify(vm); // throws on error, so do after help in case 
+                      // there are any problems 
+    } 
+    catch(po::error& e) 
+    { 
+      std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
+      std::cerr << desc << std::endl; 
+      return 1; 
+    } 
+
+
+/*****************************************
 	if(argc < 5) {
 		usage();
 		return 1;
@@ -25,6 +61,8 @@ int main( int argc, char **argv ) {
 	std::string 	header	 {argv[2]};
 	uint64_t header_row_index {std::stoull(argv[3])};
 	uint64_t  first_data_row_index {std::stoull(argv[4])};
+*****************************************/
+
 	constexpr unsigned char DELIM {','};
 	std::shared_ptr<CSVParser> t1;
 	try {
@@ -68,7 +106,7 @@ int main( int argc, char **argv ) {
 		std::cout << "******************************************" << std::endl;
 
 		uint64_t sum{};
-		for(auto cur_res : result_cds) {
+		for(auto &cur_res : result_cds) {
 			t1->getDataSegment(cur_res);
 			std::cout << cur_res << '\n' << std::endl;
 			sum += cur_res.data_summary_actual;
